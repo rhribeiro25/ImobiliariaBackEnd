47bf5dd5b6c4e876c7683868f742b024f54fb4b0
Renan Ribeiro <renan.shaolin.25@gmail.com>
2020-02-06 01:51:48 -0300

    Bug to start

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const person_controller = require('./controller/person_controller');
+const person_controller = require('./app/controller/person_controller');
 const app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017//http_app', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/http_app', {useNewUrlParser: true});
 app.use('/people', person_controller);
 //app.use('/properties', property_controller);
 
